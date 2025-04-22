0df9e9e31caa9d04f0e23c641fefb97b35296b69
Daniel Marabotto <marabotto@D-Marabotto.local>
2025-04-21 21:40:26 -0600

    Added Horizontal enemy (horizontalSpawn), with shooting,dead and explosion functionalities

---
+++ Assets/Scripts/HorizontalEnemy.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HorizontalEnemy : MonoBehaviour
+{
+    [SerializeField] private float _horizontalSpeed = 5;
+    [SerializeField] private GameObject _laser2Prefab;
+    [SerializeField] private GameObject _explosionPrefab;
+    [SerializeField] private float _fireRate = 6f;
+    [SerializeField] private float _laserSpeed = 5f;
+    private float _canFire = -1f;
+    private Player _player;
+    [SerializeField]private Animator _anim;
+    [SerializeField]private AudioSource _audioSource;
+    private bool _isDead = false;
+    private bool _isMovingRight = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _player = GameObject.Find("Player").GetComponent<Player>();
+        _anim = GetComponent<Animator>();
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        FireLaser();
+    }
+
+    void Move()
+    {
+        if (_isDead) return;
+
+        if (_isMovingRight)
+        {
+            transform.Translate(Vector3.right * _horizontalSpeed * Time.deltaTime);
+
+            if (transform.position.x >= 24f)
+            {
+                _isMovingRight = false;
+            }
+        }
+        else
+        {
+            transform.Translate(Vector3.left * _horizontalSpeed * Time.deltaTime);
+
+            if (transform.position.x <= -28f)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+    }
+
+    void FireLaser()
+    {
+        if (Time.time > _canFire &&  !_isDead)
+        {
+            _fireRate = Random.Range(2f, 5f);
+            _canFire = Time.time + _fireRate;
+            Instantiate(_laser2Prefab, transform.position, Quaternion.identity);
+            
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("HorizontalEnemy HIT" + other.name);
+        if (other.CompareTag("Laser"))
+        {
+            Destroy(other.gameObject);
+            Damage();
+        }
+    }
+
+    public void Damage()
+    {
+        if (_isDead) return;
+
+        _isDead = true;
+
+        if (_explosionPrefab != null)
+        {
+            Instantiate(_explosionPrefab, transform.position, Quaternion.identity);
+            Debug.Log("Explosion Spawned");
+        }
+
+        else
+        {
+            Debug.LogWarning("Explosion Prefab is not assigned");
+        }
+        SpriteRenderer sr = GetComponent<SpriteRenderer>();
+
+        if (sr != null) sr.enabled = false;
+
+        _horizontalSpeed = 0;
+
+        if (_audioSource != null)
+        {
+            _audioSource.Play();
+        }
+
+        if (_player != null)
+        {
+            _player.AddScore(20);
+        }
+       
+        Destroy(this.gameObject, 2.8f);
+    }
+
+
+}
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -5,6 +5,7 @@ public class SpawnManager : MonoBehaviour
 
 {
     [SerializeField]private GameObject _enemyprefab;
+    [SerializeField] private GameObject _horizontalEnemyPrefab;
     [SerializeField]private GameObject _enemyContainer;
     [SerializeField]private GameObject[] _powerups;
     private bool _stopSpawning = false;
@@ -14,6 +15,7 @@ public class SpawnManager : MonoBehaviour
     {
         StartCoroutine(SpawnEnemyRoutine());
         StartCoroutine(SpawnPowerupRoutine());
+        StartCoroutine(SpawnHorizontalEnemyRoutine());
     }
 
     IEnumerator SpawnEnemyRoutine ()
@@ -28,6 +30,22 @@ public class SpawnManager : MonoBehaviour
         }     
     }
 
+
+   IEnumerator SpawnHorizontalEnemyRoutine()
+    {
+        yield return new WaitForSeconds(15f);
+        while (_stopSpawning == false)
+        {
+            Vector3 horizontalPos = new Vector3(-24f,Random.Range(1f, 6f), 0);
+            GameObject horizontalEnemy = Instantiate(_horizontalEnemyPrefab, horizontalPos, Quaternion.identity);
+            horizontalEnemy.transform.parent = _enemyContainer.transform;
+            yield return new WaitForSeconds(35.0f);
+
+        }
+    }
+
+
+
     IEnumerator SpawnPowerupRoutine()
     {
         yield return new WaitForSeconds(3.0f);// Wait 3 seconds before starting to spawn power-ups
@@ -41,7 +59,7 @@ public class SpawnManager : MonoBehaviour
             {
                 float _rarityRoll = Random.Range(0f, 1f);// Generate a float between 0 and 1
 
-                if (_rarityRoll <= 0.15f)// Only spawn Bolt Power-Up 15% of the time
+                if (_rarityRoll <= 0.50f)// Only spawn Bolt Power-Up 15% of the time
                 {
                     Instantiate(_powerups[randomPowerUP], posToSpawn, Quaternion.identity);
                     // Spawn rare Bolt Power-Up
